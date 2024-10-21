30c16dc52d05c335904f436b2473e3e23114e01a
Kelvin Mock <kelvin.mock@gmail.com>
2024-10-21 06:12:18 -0400

    Removed unnecessary code files and unused variables

--- constants.py
+++ constants.py
@@ -14,18 +14,11 @@ TEST_SIZE = 50;
 img_height, img_width = 64, 64; # assume equal (i.e., square image)
 gamma = 0.4;
 gaussianSigma = 1;
-channels = 1; # grayscale
 
 # MLP
 num_classes = 2;
-weight_decay = 0.0001;
 batch_size = 128;
 num_epochs = 50;
-dropout_rate = 0.2;
-patch_size = (img_height, img_width);  # Size of the patches to be extracted from the input images.
-num_patches = int( (img_height // patch_size[0]) * ((img_width // patch_size[1])) );  # Size of the data array.
-embedding_dim = 256;  # Number of hidden units.
-num_blocks = 4;  # Number of blocks.
 learning_rate = 0.005;
 input_shape = (int(img_height),);
 
